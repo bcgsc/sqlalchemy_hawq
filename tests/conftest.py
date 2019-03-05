8cbc52816b91fa840612669486e449ef15595f39
Eleanor Lewis <elewis@elewis01.phage.bcgsc.ca>
2019-03-05 12:35:38 -0800

    copies linted conftest.py from fk-tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,18 @@
+"""
+Defines pytest command line arguments and fixtures that can be used throughout tests.
+"""
 import pytest
-from sqlalchemy.engine.url import URL
-import hawq_sqlalchemy
 from sqlalchemy.exc import ProgrammingError
 from sqlalchemy import create_engine
-
 from sqlalchemy.orm import configure_mappers
 from sqlalchemy.schema import CreateSchema
+from sqlalchemy.engine.url import URL
+
+
 def pytest_addoption(parser):
+    """
+    Defines possible command line arguments
+    """
     parser.addoption("--username", action="store", default=None, help="gateway username")
     parser.addoption("--password", action="store", default=None, help="password for gateway user")
     parser.addoption("--echo", action="store", default=False, help="echo commands to db")
@@ -14,22 +20,33 @@ def pytest_addoption(parser):
 
 @pytest.fixture(scope='session')
 def username(request):
-    un = request.config.getoption("--username")
-    if un is None:
+    """
+    Returns username passed as command line arg
+    """
+    usern = request.config.getoption("--username")
+    if usern is None:
         print("Skipped test requiring username or password")
         pytest.skip()
-    return un
+    return usern
+
 
 @pytest.fixture(scope='session')
 def password(request):
+    """
+    Returns password passed as command line arg
+    """
     passw = request.config.getoption("--password")
     if passw is None:
         print("Skipped test requiring username or password")
         pytest.skip()
     return passw
 
+
 @pytest.fixture(scope='session')
 def echo_sql(request):
+    """
+    Returns 'echo' passed as command line arg
+    """
     echo = request.config.getoption("--echo")
     if echo is False:
         return False
@@ -38,16 +55,19 @@ def echo_sql(request):
 
 @pytest.fixture(scope='session')
 def test_engine(request, username, password, echo_sql):
+    """
+    Returns engine connected to hawq database with schema testschema initialized
+    """
     host = 'hdp-master02.hadoop.bcgsc.ca'
     port = '5432'
     database = 'test_refactor'
     drivername = 'hawq'
     url = URL(host=host,
-                database=database,
-                username=username,
-                password=password,
-                drivername=drivername,
-                port=port)
+              database=database,
+              username=username,
+              password=password,
+              drivername=drivername,
+              port=port)
 
     engine = create_engine(url, echo=echo_sql)
 
@@ -75,4 +95,3 @@ def test_engine(request, username, password, echo_sql):
             print("Unexpected error:", prog_e.orig)
             return """
     engine.dispose()
-
