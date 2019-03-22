a4a26d3f7ee9751ef42e0b9073f5d5b43f9c8083
Eleanor Lewis <elewis@elewis01.phage.bcgsc.ca>
2019-03-22 16:23:38 -0700

    Change requirements so all tests are pass or skip

--- test/conftest.py
+++ test/conftest.py
@@ -6,3 +6,4 @@ from sqlalchemy.dialects import registry
 registry.register('hawq', 'hawq_sqlalchemy.dialect', 'HawqDialect')
 registry.register('hawq+psycopq2', 'hawq_sqlalchemy.dialect', 'HawqDialect')
 from sqlalchemy.testing.plugin.pytestplugin import *
+
--- test/requirements.py
+++ test/requirements.py
@@ -22,3 +22,23 @@ class Requirements(SuiteRequirements):
     @property
     def table_reflection(self):
         return exclusions.closed()
+
+    
+    @property
+    def implicitly_named_constraints(self):
+        """target database must apply names to unnamed constraints."""
+        return exclusions.closed()
+
+    @property
+    def parens_in_union_contained_select_w_limit_offset(self):
+        return exclusions.closed()
+
+    @property
+    def parens_in_union_contained_select_wo_limit_offset(self):
+        return exclusions.closed()
+
+    
+
+    @property
+    def reflects_pk_names(self):
+        return exclusions.open()
